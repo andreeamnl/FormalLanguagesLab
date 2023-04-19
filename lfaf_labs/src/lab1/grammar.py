ecdef0b88101a7419b9b6d2eff921520c95fbc5b
OzzyOsbourne <90449002+andreeamnl@users.noreply.github.com>
2023-04-19 21:09:21 +0300

    fix typo

--- lfaf_labs/src/lab1/grammar.py
+++ lfaf_labs/src/lab1/grammar.py
@@ -17,6 +17,7 @@ class Grammar:
                     self.terminals.add(symbol)
 
         self.start_symbol = list(self.grammar.keys())[0]
+        self.terminals={'a', 'b'}
 
     def generate(self, start_symbol):
             rule = self.grammar.get(start_symbol)
@@ -87,3 +88,156 @@ class Grammar:
                     if symbol[0] not in self.lowc:
                         return False
         return True
+    
+    def remove_epsilon_prods(self):
+        nulls = set()
+        for var, prods in self.grammar.items():
+            if "e" in prods:
+                nulls.add(var)
+
+        new_prods = {}
+        for var, prods in self.grammar.items():
+            new_prods[var] = []
+            for prod in prods:
+                if prod == "e":
+                    continue
+                for nullable in nulls:
+                    if nullable in prod:
+                        new_prod = prod.replace(nullable, "")
+                        if new_prod not in new_prods[var]:
+                            new_prods[var].append(new_prod)
+                if prod not in new_prods[var]:
+                    new_prods[var].append(prod)
+
+        for var, prods in new_prods.items():
+            new_prods[var] = [p for p in prods if p != "e"]
+
+        for var, prods in new_prods.items():
+            for prod in prods:
+                if len(prod) == 1 and prod in nulls:
+                    continue
+                for i in range(len(prod)):
+                    if prod[i] in nulls:
+                        new_prod = prod[:i] + prod[i + 1:]
+                        if new_prod not in new_prods[var]:
+                            new_prods[var].append(new_prod)
+
+        self.grammar = {k: v for k, v in new_prods.items() if v}        
+        return self.grammar
+
+    def remove_unit_prods(self):
+        for symbol in self.grammar:
+            unit_prods = [prod for prod in self.grammar[symbol] if len(prod) == 1 and prod.isupper()]
+            while unit_prods:
+                unit = unit_prods.pop(0)
+                self.grammar[symbol].remove(unit)
+                self.grammar[symbol].extend(
+                    prod for prod in self.grammar[unit] if prod not in self.grammar[symbol])
+                unit_prods = [prod for prod in self.grammar[symbol] if len(prod) == 1 and prod.isupper()]
+        #print(self.grammar)        
+        return self.grammar
+    
+
+    def remove_inaccessible_symbols(self):
+        def visit(symbol, visited):
+            if symbol not in visited:
+                visited.add(symbol)
+                for prod in self.grammar[symbol]:
+                    for s in prod:
+                        if s in self.non_terminals:
+                            visit(s, visited)
+
+        accessible_symbols = set()
+        visit(self.start_symbol, accessible_symbols)
+
+        self.non_terminals = [nt for nt in self.non_terminals if nt in accessible_symbols]
+        self.grammar = {k: v for k, v in self.grammar.items() if k in accessible_symbols}
+        #print(self.grammar)        
+        return self.grammar
+
+    def remove_nonproductive(self):
+        productive = {self.start_symbol}
+        old_productive = set()
+
+        while old_productive != productive:
+            old_productive = productive.copy()
+            for symbol, rhs in self.grammar.items():
+                if symbol not in productive:
+                    for prod in rhs:
+                        if all(s in productive or s in self.terminals for s in prod):
+                            productive.add(symbol)
+
+        nonproductive = set(self.non_terminals) - productive
+
+        for symbol in nonproductive:
+            del self.grammar[symbol]
+
+        for symbol, rhs in self.grammar.items():
+            new_rhs = []
+            for prod in rhs:
+                if all(s in productive or s in self.terminals for s in prod):
+                    new_rhs.append(prod)
+            self.grammar[symbol] = new_rhs
+
+        self.non_terminals = sorted(list(productive))
+        #print(self.grammar)        
+        return self.grammar
+
+
+    def to_cnf(self):
+        new_prods = {}
+        next_new_var = 1
+        terminal_var_map = {}
+
+        for var, prods in self.grammar.items():
+            new_prods[var] = []
+
+            for prod in prods:
+                if len(prod) >= 3:
+                    prod_vars = [f"X{next_new_var + i}" for i in range(len(prod) - 1)]
+                    next_new_var += len(prod) - 1
+                    self.non_terminals.extend(prod_vars)
+
+                    new_prods[var].append(prod[0] + prod_vars[0])
+                    for i in range(len(prod) - 2):
+                        new_var = prod_vars[i]
+                        new_prods.setdefault(new_var, [])
+                        new_prods[new_var].append(prod[i + 1] + prod_vars[i + 1])
+                    new_prods.setdefault(prod_vars[-1], [])
+                    new_prods[prod_vars[-1]].append(prod[-1])
+
+                elif len(prod) == 2 and all(sym in self.non_terminals for sym in prod):
+                    new_prods[var].append(prod)
+
+                else:
+                    new_prod = prod
+                    for sym in prod:
+                        if sym in self.terminals:
+                            if sym not in terminal_var_map:
+                                new_var = f"T{next_new_var}"
+                                next_new_var += 1
+                                self.non_terminals.append(new_var)
+                                new_prods.setdefault(new_var, [])
+                                new_prods[new_var].append(sym)
+                                terminal_var_map[sym] = new_var
+                            new_prod = new_prod.replace(sym, terminal_var_map[sym], 1)
+                    new_prods[var].append(new_prod)
+
+        self.grammar = new_prods
+        self.non_terminals = sorted(list(set(self.non_terminals)))
+        ##print(self.grammar)        
+        return self.grammar
+
+    def print_grammar(self):
+        for var, prods in self.grammar.items():
+            
+            print(f"{var} :", "  ".join(prods))
+
+    def cfg_to_cnf(self):
+        self.remove_epsilon_prods()
+        self.remove_unit_prods()
+        self.remove_inaccessible_symbols()
+        self.remove_nonproductive()
+        self.to_cnf()
+        self.print_grammar()
+        return self
