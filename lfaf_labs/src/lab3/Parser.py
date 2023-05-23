032b7b5a71e924a8eb76c372bd5c9eb53ea210d3
OzzyOsbourne <90449002+andreeamnl@users.noreply.github.com>
2023-05-23 15:20:53 +0300

    Add commentary

--- lfaf_labs/src/lab3/Parser.py
+++ lfaf_labs/src/lab3/Parser.py
@@ -1,69 +1,8 @@
-#######################################
-# IMPORTS
-#######################################
+from lexer import Lexer
+from token import token
 
-#from strings_with_arrows import *
 
-#######################################
-# CONSTANTS
-#######################################
-
-DIGITS = '0123456789'
-
-#######################################
-# ERRORS
-#######################################
-def string_with_arrows(text, pos_start, pos_end):
-    result = ''
-
-    # Calculate indices
-    idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
-    idx_end = text.find('\n', idx_start + 1)
-    if idx_end < 0: idx_end = len(text)
-    
-    # Generate each line
-    line_count = pos_end.ln - pos_start.ln + 1
-    for i in range(line_count):
-        # Calculate line columns
-        line = text[idx_start:idx_end]
-        col_start = pos_start.col if i == 0 else 0
-        col_end = pos_end.col if i == line_count - 1 else len(line) - 1
-
-        # Append to result
-        result += line + '\n'
-        result += ' ' * col_start + '^' * (col_end - col_start)
-
-        # Re-calculate indices
-        idx_start = idx_end
-        idx_end = text.find('\n', idx_start + 1)
-        if idx_end < 0: idx_end = len(text)
-
-    return result.replace('\t', '')
-
-class Error:
-		def __init__(self, pos_start, pos_end, error_name, details):
-				self.pos_start = pos_start
-				self.pos_end = pos_end
-				self.error_name = error_name
-				self.details = details
-		
-		def as_string(self):
-				result  = f'{self.error_name}: {self.details}\n'
-				result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
-				result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
-				return result
-
-class IllegalCharError(Error):
-		def __init__(self, pos_start, pos_end, details):
-				super().__init__(pos_start, pos_end, 'Illegal Character', details)
-
-class InvalidSyntaxError(Error):
-		def __init__(self, pos_start, pos_end, details=''):
-				super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
-
-#######################################
-# POSITION
-#######################################
+# position
 
 class Position:
 		def __init__(self, idx, ln, col, fn, ftxt):
@@ -86,110 +25,9 @@ class Position:
 		def copy(self):
 				return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
-#######################################
-# TOKENS
-#######################################
 
-TT_INT			= 'INT'
-TT_FLOAT    = 'FLOAT'
-TT_PLUS     = 'PLUS'
-TT_MINUS    = 'MINUS'
-TT_MUL      = 'MUL'
-TT_DIV      = 'DIV'
-TT_LPAREN   = 'LPAREN'
-TT_RPAREN   = 'RPAREN'
-TT_EOF			= 'EOF'
 
-class Token:
-		def __init__(self, type_, value=None, pos_start=None, pos_end=None):
-				self.type = type_
-				self.value = value
-
-				if pos_start:
-					self.pos_start = pos_start.copy()
-					self.pos_end = pos_start.copy()
-					self.pos_end.advance()
-
-				if pos_end:
-					self.pos_end = pos_end
-		
-		def __repr__(self):
-				if self.value: return f'{self.type}:{self.value}'
-				return f'{self.type}'
-
-#######################################
-# LEXER
-#######################################
-
-class Lexer:
-		def __init__(self, fn, text):
-				self.fn = fn
-				self.text = text
-				self.pos = Position(-1, 0, -1, fn, text)
-				self.current_char = None
-				self.advance()
-		
-		def advance(self):
-				self.pos.advance(self.current_char)
-				self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
-
-		def make_tokens(self):
-				tokens = []
-
-				while self.current_char != None:
-						if self.current_char in ' \t':
-								self.advance()
-						elif self.current_char in DIGITS:
-								tokens.append(self.make_number())
-						elif self.current_char == '+':
-								tokens.append(Token(TT_PLUS, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '-':
-								tokens.append(Token(TT_MINUS, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '*':
-								tokens.append(Token(TT_MUL, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '/':
-								tokens.append(Token(TT_DIV, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == '(':
-								tokens.append(Token(TT_LPAREN, pos_start=self.pos))
-								self.advance()
-						elif self.current_char == ')':
-								tokens.append(Token(TT_RPAREN, pos_start=self.pos))
-								self.advance()
-						else:
-								pos_start = self.pos.copy()
-								char = self.current_char
-								self.advance()
-								return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
-
-				tokens.append(Token(TT_EOF, pos_start=self.pos))
-				return tokens, None
-
-		def make_number(self):
-				num_str = ''
-				dot_count = 0
-				pos_start = self.pos.copy()
-
-				while self.current_char != None and self.current_char in DIGITS + '.':
-						if self.current_char == '.':
-								if dot_count == 1: break
-								dot_count += 1
-								num_str += '.'
-						else:
-								num_str += self.current_char
-						self.advance()
-
-				if dot_count == 0:
-						return Token(TT_INT, int(num_str), pos_start, self.pos)
-				else:
-						return Token(TT_FLOAT, float(num_str), pos_start, self.pos)
-
-#######################################
-# NODES
-#######################################
+# nodes
 
 class NumberNode:
 	def __init__(self, tok):
@@ -215,9 +53,7 @@ class UnaryOpNode:
 	def __repr__(self):
 		return f'({self.op_tok}, {self.node})'
 
-#######################################
-# PARSE RESULT
-#######################################
+# result
 
 class ParseResult:
 	def __init__(self):
@@ -239,9 +75,7 @@ class ParseResult:
 		self.error = error
 		return self
 
-#######################################
-# PARSER
-#######################################
+# parser
 
 class Parser:
 	def __init__(self, tokens):
@@ -264,8 +98,7 @@ class Parser:
 			))
 		return res
 
-	###################################
-
+	
 	def factor(self):
 		res = ParseResult()
 		tok = self.current_tok
@@ -320,27 +153,51 @@ class Parser:
 
 		return res.success(left)
 
-#######################################
-# RUN
-#######################################
+# errors
+def string_with_arrows(text, pos_start, pos_end):
+    result = ''
 
-def run(fn, text):
-		# Generate tokens
-		lexer = Lexer(fn, text)
-		tokens, error = lexer.make_tokens()
-		if error: return None, error
+    # Calculate indices
+    idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
+    idx_end = text.find('\n', idx_start + 1)
+    if idx_end < 0: idx_end = len(text)
+    
+    # Generate each line
+    line_count = pos_end.ln - pos_start.ln + 1
+    for i in range(line_count):
+        # Calculate line columns
+        line = text[idx_start:idx_end]
+        col_start = pos_start.col if i == 0 else 0
+        col_end = pos_end.col if i == line_count - 1 else len(line) - 1
+
+        # Append to result
+        result += line + '\n'
+        result += ' ' * col_start + '^' * (col_end - col_start)
+
+        # Re-calculate indices
+        idx_start = idx_end
+        idx_end = text.find('\n', idx_start + 1)
+        if idx_end < 0: idx_end = len(text)
+
+    return result.replace('\t', '')
+
+class Error:
+		def __init__(self, pos_start, pos_end, error_name, details):
+				self.pos_start = pos_start
+				self.pos_end = pos_end
+				self.error_name = error_name
+				self.details = details
 		
-		# Generate AST
-		parser = Parser(tokens)
-		ast = parser.parse()
+		def as_string(self):
+				result  = f'{self.error_name}: {self.details}\n'
+				result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
+				result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+				return result
 
-		return ast.node, ast.error
+class IllegalCharError(Error):
+		def __init__(self, pos_start, pos_end, details):
+				super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
-
-
-while True:
-		text = input('shell > ')
-		result, error = run('<stdin>', text)
-
-		if error: print(error.as_string())
-		else: print(result)
+class InvalidSyntaxError(Error):
+		def __init__(self, pos_start, pos_end, details=''):
+				super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
